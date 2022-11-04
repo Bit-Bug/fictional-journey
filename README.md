c0d41b4cb0a25aee20a8462f39f4f50fa7c3d591
Bit-Bug <bitbug.devlab@gmail.com>
2022-11-04 20:17:06 +0100

    1 step ahead

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fictional-journey
+-- A learning project that covers infrastructure as a code for a serverless backend project --
+
+# Setup
+
+## npm init -y 
---
+++ infra/Launcher.ts
@@ -0,0 +1,8 @@
+import { App } from "aws-cdk-lib";
+import { SpaceStack } from "./spaceStack";
+
+const app = new App();
+
+new SpaceStack(app, 'fictional-journey', {
+
+})
